af6212829d807658c3f219eb986d97919e78f496
x <x>
2024-05-07 10:29:30 +0200

    Improve slippage

--- src/hooks/useSettings.ts
+++ src/hooks/useSettings.ts
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 export default function useSettings() {
     const [maxSlippagePercent] = useState<Percent>(
-        new Percent(10, 10_000),
+        new Percent(100, 10_000),
     );
 
     return { maxSlippagePercent };
