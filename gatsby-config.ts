aa543acce653757e093935b25541fd6927ff68a8
deepakkurmi5 <deepakkurmi874@gmail.com>
2024-05-21 23:07:41 +0530

    fixing bugs

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,19 +1,16 @@
 import type { GatsbyConfig } from 'gatsby';
 
 require('dotenv').config({
-    path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.${process.env.NODE_ENV}`,
 });
 
 const config: GatsbyConfig = {
-    siteMetadata: {
-        title: 'dapp',
-        siteUrl: 'https://www.saber.so',
-    },
-    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-    // If you use VSCode you can also use the GraphQL plugin
-    // Learn more at: https://gatsby.dev/graphql-typegen
-    graphqlTypegen: true,
-    plugins: ['gatsby-plugin-postcss'],
+  siteMetadata: {
+    title: 'dapp',
+    siteUrl: 'https://www.saber.so',
+  },
+  graphqlTypegen: true,
+  plugins: ['gatsby-plugin-postcss'],
 };
 
 export default config;
