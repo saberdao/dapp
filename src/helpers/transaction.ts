fbbb6e83bde64ce1c8fd2d607e00e8fc7ebbd0f1
x <x>
2024-07-03 11:39:27 +0200

    Fix stake right after deposit sometimes not working

--- src/helpers/transaction.ts
+++ src/helpers/transaction.ts
@@ -13,7 +13,10 @@ const getCUsForTx = async (
     }).compileToV0Message();
     const transaction = new VersionedTransaction(messageV0);
     const simulation = await connection.simulateTransaction(transaction);
-    const CUs = simulation.value.unitsConsumed ? Math.ceil(1.1 * simulation.value.unitsConsumed) : 1.4e6;
+
+    // Add 25K + 10% leeway here because the simulation of a stake right after deposit takes some time (>10-20 secs) to update to the correct
+    // number of CUs used (and maybe other TXs as well). Just 10% is not enough for very low CU TXs (like quarry stake).
+    const CUs = simulation.value.unitsConsumed ? Math.ceil(1.1 * simulation.value.unitsConsumed + 25000) : 1.4e6;
     return CUs;
 };
 
@@ -24,6 +27,7 @@ export const createVersionedTransaction = async (
 ) => {
     const latestBlockhash = await connection.getLatestBlockhash('finalized');
     const CUs = await getCUsForTx(connection, latestBlockhash, txs, payerKey);
+    console.log(CUs)
 
     const priorityFeeLS = parseFloat(localStorage.getItem('priorityFee') ?? '');
     const priorityFee = (priorityFeeLS || 0) * LAMPORTS_PER_SOL * 1e6;
