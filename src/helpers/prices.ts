7d9715ff99e8457957a609022cff8eccc975bba0
x <x>
2024-11-08 11:09:05 +0100

    Fix price api

--- src/helpers/prices.ts
+++ src/helpers/prices.ts
@@ -1,70 +1,77 @@
-import { PoolData } from '../types';
+import { PoolData } from "../types";
 
 const blacklist = [
-    'CowKesoLUaHSbAMaUxJUj7eodHHsaLsS65cy8NFyRDGP',
-    'LUN9joXyojPTbKf3HwcHw12PKMLrmzEMrUSAinUGKr5',
-    'SLNAAQ8VT6DRDc3W9UPDjFyRt7u4mzh8Z4WYMDjJc35',
-]
+  "CowKesoLUaHSbAMaUxJUj7eodHHsaLsS65cy8NFyRDGP",
+  "LUN9joXyojPTbKf3HwcHw12PKMLrmzEMrUSAinUGKr5",
+  "SLNAAQ8VT6DRDc3W9UPDjFyRt7u4mzh8Z4WYMDjJc35",
+];
 
-export const getPoolTVL = (pool: Omit<PoolData, 'metrics'>) => {
-    // Get amount token A and B, and their USD prices, and calculate TVL
-    const amountTokenA = pool.exchangeInfo.reserves[0].amount;
-    const amountTokenB = pool.exchangeInfo.reserves[1].amount;
+export const getPoolTVL = (pool: Omit<PoolData, "metrics">) => {
+  // Get amount token A and B, and their USD prices, and calculate TVL
+  const amountTokenA = pool.exchangeInfo.reserves[0].amount;
+  const amountTokenB = pool.exchangeInfo.reserves[1].amount;
 
-    const usdPriceA = pool.usdPrice.tokenA;
-    const usdPriceB = pool.usdPrice.tokenB;
+  const usdPriceA = pool.usdPrice.tokenA;
+  const usdPriceB = pool.usdPrice.tokenB;
 
-    const tvl = amountTokenA.asNumber * usdPriceA + amountTokenB.asNumber * usdPriceB;
-    return tvl;
+  const tvl =
+    amountTokenA.asNumber * usdPriceA + amountTokenB.asNumber * usdPriceB;
+  return tvl;
 };
 
-export const getPoolTokenPercentages = (pool: Omit<PoolData, 'metrics'>) => {
-    // Get amount token A and B, and their USD prices, and calculate TVL
-    const amountTokenA = pool.exchangeInfo.reserves[0].amount.asNumber;
-    const amountTokenB = pool.exchangeInfo.reserves[1].amount.asNumber;
+export const getPoolTokenPercentages = (pool: Omit<PoolData, "metrics">) => {
+  // Get amount token A and B, and their USD prices, and calculate TVL
+  const amountTokenA = pool.exchangeInfo.reserves[0].amount.asNumber;
+  const amountTokenB = pool.exchangeInfo.reserves[1].amount.asNumber;
 
-    return {
-        tokenA: amountTokenA / (amountTokenA + amountTokenB),
-        tokenB: amountTokenB / (amountTokenA + amountTokenB),
-    };
+  return {
+    tokenA: amountTokenA / (amountTokenA + amountTokenB),
+    tokenB: amountTokenB / (amountTokenA + amountTokenB),
+  };
 };
 
 const priceCache: Record<string, number> = {};
 export const getPrice = async (mint: string, decimals: number) => {
-    if (blacklist.includes(mint)) {
-        return 0;
-    }
+  if (blacklist.includes(mint)) {
+    return 0;
+  }
 
-    if (priceCache[mint]) {
-        return priceCache[mint];
-    }
-
-    try {
-        const result = await fetch(`https://price.jup.ag/v4/price?ids=${mint}`).then(res => res.json());
-        if (!result.data) {
-            throw Error('No data');
-        }
-        Object.values(result.data).forEach((priceRecord: any) => {
-            priceCache[priceRecord.id] = priceRecord.price;
-        });
-    } catch (e) {
-        // Try using quote
-        try {
-            const quote = await fetch(`https://quote-api.jup.ag/v6/quote?inputMint=${mint}&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=${10 ** decimals}`).then(res => res.json());
-
-            if (quote?.outAmount) {
-                priceCache[mint] = parseInt(quote.outAmount) / (10 ** 6);
-            } else {
-                priceCache[mint] = 0;
-            }
-        } catch(e) {
-            priceCache[mint] = 0;
-        }
-    }
-
-    if (!priceCache[mint]) {
-        priceCache[mint] = 0;
-    }
-
+  if (priceCache[mint]) {
     return priceCache[mint];
+  }
+
+  try {
+    const result = await fetch(`https://api.jup.ag/price/v2?ids=${mint}`).then(
+      (res) => res.json()
+    );
+    if (!result.data) {
+      throw Error("No data");
+    }
+    Object.values(result.data).forEach((priceRecord: any) => {
+      priceCache[priceRecord.id] = parseFloat(priceRecord.price);
+    });
+  } catch (e) {
+    // Try using quote
+    try {
+      const quote = await fetch(
+        `https://quote-api.jup.ag/v6/quote?inputMint=${mint}&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=${
+          10 ** decimals
+        }`
+      ).then((res) => res.json());
+
+      if (quote?.outAmount) {
+        priceCache[mint] = parseInt(quote.outAmount) / 10 ** 6;
+      } else {
+        priceCache[mint] = 0;
+      }
+    } catch (e) {
+      priceCache[mint] = 0;
+    }
+  }
+
+  if (!priceCache[mint]) {
+    priceCache[mint] = 0;
+  }
+
+  return priceCache[mint];
 };
--- src/hooks/useGetPrices.ts
+++ src/hooks/useGetPrices.ts
@@ -1,77 +1,96 @@
-import { PythHttpClient, getPythProgramKeyForCluster } from '@pythnetwork/client';
-import { useConnection } from '@solana/wallet-adapter-react';
-import { useQuery } from '@tanstack/react-query';
-import useNetwork from '../hooks/useNetwork';
-import { OraclePrice } from '../types';
-import { chunk } from 'lodash';
-import useGetPools from './useGetPools';
-import { SBR_MINT } from '@saberhq/saber-periphery';
+import {
+  PythHttpClient,
+  getPythProgramKeyForCluster,
+} from "@pythnetwork/client";
+import { useConnection } from "@solana/wallet-adapter-react";
+import { useQuery } from "@tanstack/react-query";
+import useNetwork from "../hooks/useNetwork";
+import { OraclePrice } from "../types";
+import { chunk } from "lodash";
+import useGetPools from "./useGetPools";
+import { SBR_MINT } from "@saberhq/saber-periphery";
 
 export default function useGetPrices() {
-    const { formattedNetwork } = useNetwork();
-    const { connection } = useConnection();
-    const { data: poolData } = useGetPools(formattedNetwork);
-    const { network, endpoint } = useNetwork();
+  const { formattedNetwork } = useNetwork();
+  const { connection } = useConnection();
+  const { data: poolData } = useGetPools(formattedNetwork);
+  const { network, endpoint } = useNetwork();
 
-    const pools = poolData?.pools;
+  const pools = poolData?.pools;
 
-    return useQuery({
-        queryKey: ['prices', endpoint, (pools ?? []).length > 0 ? 'y' : 'n'],
-        queryFn: async () => {
-            if (!pools) {
-                return null;
+  return useQuery({
+    queryKey: ["prices", endpoint, (pools ?? []).length > 0 ? "y" : "n"],
+    queryFn: async () => {
+      try {
+        if (!pools) {
+          return null;
+        }
+
+        // Get prices from pyth
+        const client = new PythHttpClient(
+          connection,
+          getPythProgramKeyForCluster(network)
+        );
+        const pythData = await client.getData();
+
+        let tryPrice = 0;
+        try {
+          const cgData = await fetch(
+            "https://api.coingecko.com/api/v3/simple/price?ids=bilira&vs_currencies=usd"
+          );
+          tryPrice = (await cgData.json())?.bilira?.usd ?? 0;
+        } catch (e) {
+          // Do nothing (just assume is 0)
+        }
+
+        // We can add more from pyth here later
+        const prices: OraclePrice = {};
+
+        // Get the rest from Jupiter for now, until all Pyth oracles are defined.
+        const oraclePriceMints = Object.keys(prices);
+        const allContractMints = pools
+          .map((pool) => {
+            return [pool.tokens[0].address, pool.tokens[1].address];
+          })
+          .flat()
+          .filter((address) => !oraclePriceMints.includes(address))
+          .concat(SBR_MINT);
+
+        // Chunk them per 100 (Jup limit)
+        const chunks = chunk(allContractMints, 100);
+
+        // Call Jup price api for each chunk
+        const result = (
+          await Promise.all(
+            chunks.map(async (chunk) => {
+              try {
+                return await fetch(
+                  `https://api.jup.ag/price/v2?ids=${chunk.join(",")}`
+                ).then((res) => res.json());
+              } catch (e) {
+                return {};
+              }
+            })
+          )
+        ).flat();
+
+        // Merge the results into prices
+        result.forEach((item: any) => {
+          Object.values(item?.data ?? {}).forEach((priceRecord: any) => {
+            if (!priceRecord) {
+              return;
             }
+            prices[priceRecord.id] = parseFloat(priceRecord.price);
+          });
+        });
+        console.log(prices);
 
-            // Get prices from pyth
-            const client = new PythHttpClient(connection, getPythProgramKeyForCluster(network));
-            const pythData = await client.getData();
-
-            let tryPrice = 0;
-            try {
-                const cgData = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bilira&vs_currencies=usd')
-                tryPrice = (await cgData.json())?.bilira?.usd ?? 0;
-            } catch (e) {
-                // Do nothing (just assume is 0)
-            }
-
-            // We can add more from pyth here later
-            const prices: OraclePrice = {};
-
-            // Get the rest from Jupiter for now, until all Pyth oracles are defined.
-            const oraclePriceMints = Object.keys(prices);
-            const allContractMints = pools
-                .map(pool => {
-                    return [
-                        pool.tokens[0].address,
-                        pool.tokens[1].address,
-                    ];
-                })
-                .flat()
-                .filter(address => !oraclePriceMints.includes(address))
-                .concat(SBR_MINT);
-
-            // Chunk them per 100 (Jup limit)
-            const chunks = chunk(allContractMints, 100);
-
-            // Call Jup price api for each chunk
-            const result = (await Promise.all(chunks.map(async (chunk) => {
-                try {
-                    return await fetch(`https://price.jup.ag/v4/price?ids=${chunk.join(',')}`).then(res => res.json());
-                } catch (e) {
-                    return {};
-                }
-            }))).flat();
-
-            // Merge the results into prices
-            result.forEach((item: any) => {
-                Object.values(item?.data ?? {}).forEach((priceRecord: any) => {
-                    prices[priceRecord.id] = priceRecord.price;
-                });
-            });
-            console.log(prices)
-
-            return prices;
-        },
-        staleTime: 1000 * 600,
-    });
+        return prices;
+      } catch (e) {
+        console.error(e);
+        return null;
+      }
+    },
+    staleTime: 1000 * 600,
+  });
 }
